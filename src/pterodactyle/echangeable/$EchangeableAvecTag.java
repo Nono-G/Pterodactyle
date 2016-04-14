fb44ae9a0040ecd03ca54e22096563546672f1fa
Nono-G <noe.goudian@e.ujf-grenoble.fr>
2016-04-14 16:18:14 +0200

    Nono : divers trucs echangeables

---
+++ src/pterodactyle/echangeable/$EchangeableAvecTag.java
@@ -0,0 +1,9 @@
+package pterodactyle.echangeable;
+
+import java.util.List;
+
+public abstract class $EchangeableAvecTag extends $Echangeable{
+
+	//Invariant de classe : tags non vide
+	private List<Tag> tags;//La liste des tags par lesquels l'échangeable est marqué
+}
---
+++ src/pterodactyle/echangeable/_Echangeable.java
@@ -0,0 +1,7 @@
+package pterodactyle.echangeable;
+
+
+//Interface définissant les méthodes attendues sur un échangeable
+public interface _Echangeable {
+	
+}
