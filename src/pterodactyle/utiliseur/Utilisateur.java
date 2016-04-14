b76ee73fa4b8669b8e089b363190fbd52fd5fa1d
Anasse Berahab <anasse.berahab@gmail.com>
2016-04-14 15:54:04 +0200

    Utilisateur (juste constructeur)

--- src/pterodactyle/utiliseur/Utilisateur.java
+++ src/pterodactyle/utiliseur/Utilisateur.java
@@ -1,5 +1,21 @@
 package pterodactyle.utiliseur;
 
 public class Utilisateur {
-
+	//Attributs de base de l'utilisateur
+	private String nom; 
+	private String prenom; 
+	private String login; 
+	private String motDePasse; 
+	
+	
+	/*
+	 * Constructeur basique 
+	 * @param : Nom, Prenom, Login, motDePasse
+	 */
+	public Utilisateur(String nom, String prenom, String login, String motDePasse){
+		this.nom = nom;
+		this.prenom = prenom; 
+		this.login = login; 
+		this.motDePasse = motDePasse; 
+	}
 }
