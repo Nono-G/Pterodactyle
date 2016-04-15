863e2b57543c72c66378565b735ea2162489f7f4
silvemax <silvemax@silvemax-K56CB>
2016-04-15 10:34:26 +0200

    ajout Bdd

---
+++ src/pterodactyle/serveur/Utilisateur.java
@@ -0,0 +1,18 @@
+package pterodactyle.serveur;
+
+public class Utilisateur {
+	
+	private int id;
+	private int age;
+	private String nom;
+	private String prenom;
+	
+	public Utilisateur(int id,int age, String nom, String prenom) {
+		super();
+		this.id = id;
+		this.age = age;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+}
