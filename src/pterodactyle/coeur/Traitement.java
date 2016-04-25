ca5df2c723333a90e585a1fec86b1e6581fee8fd
silvemax <silvemax@silvemax-K56CB>
2016-04-25 15:37:16 +0200

    Ajout package coeur et class traitement

--- src/pterodactyle/coeur/Traitement.java
+++ src/pterodactyle/coeur/Traitement.java
@@ -7,5 +7,16 @@ import pterodactyle.utilisateur.Utilisateur;
 
 public class Traitement {
 	
-	private HashMap< String , Utilisateur > listeUtilisateurs = new HashMap< String , Utilisateur >();
+	private static HashMap< String , Utilisateur > listeUtilisateurs = new HashMap< String , Utilisateur >();
+	
+	
+	public Traitement(){}
+	
+	public boolean estUtilisateur(String login, String mdp){
+		Utilisateur utilisateur = listeUtilisateurs.get(login);
+		if( utilisateur != null){
+			return mdp == utilisateur.getMdp();
+		}
+		return false;
+	}
 }
