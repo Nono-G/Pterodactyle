02f1ac8f751cbae9ca13a899f52f4265f64d4ecd
Nono-G <noe.goudian@e.ujf-grenoble.fr>
2016-04-26 14:44:20 +0200

    des trucs

--- src/pterodactyle/coeur2/_ServicesCoeur.java
+++ src/pterodactyle/coeur2/_ServicesCoeur.java
@@ -1,7 +1,21 @@
 package pterodactyle.coeur2;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import pterodactyle.utilisateur.Utilisateur;
 
 public interface _ServicesCoeur extends Remote {
 
+	
+	public void creerUtilisateur(Utilisateur nouveau, String identificateur, String cle);
+	
+	public Utilisateur utilisateurCourant(String login, String motDePasse)throws RemoteException;
+	
+
+	public String voirUtilisateur(String login, Utilisateur utilisateurCourant)throws RemoteException;
+
+	
+	public Object[] trancheFichier(String url, int n, int tailleTampon, Utilisateur utilisateurCourant) throws RemoteException;
+	
 }
