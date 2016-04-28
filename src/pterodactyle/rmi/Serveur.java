ac0bfa4cf67346ccd6664614da2a8978053729d8
Maxime Silvestre <maxime.silvestre.26@gmail.com>
2016-04-28 13:52:56 +0200

    rmi marche !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/pterodactyle/rmi/Serveur.java
+++ src/pterodactyle/rmi/Serveur.java
@@ -9,33 +9,52 @@ import java.rmi.RMISecurityManager;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 import pterodactyle.coeur2.CoeurBase;
+import pterodactyle.coeur2._ServicesCoeur;
 
 // rmic pterodactyle.rmi.ServicesRmiImpl
 
 public class Serveur {
+	
+	private static String hostname = "193.48.34.19";
 
 	public static void main(String[] args) throws IOException {
 
-		LocateRegistry.createRegistry(1099);
+		// courant
 		String cwd = System.getProperty("user.dir");
+		System.out.println("PWD = " + cwd);
 
-		System.setProperty("java.rmi.server.codebase", "file://" + cwd + "/");
-		System.setProperty("java.rmi.server.hostname", "");
-		System.setProperty("java.security.policy", "file://" + cwd + "/bin/policy");
-		String url = "rmi://" + InetAddress.getLocalHost().getHostAddress() + ":1099/app";
-		//String url = "rmi://192.168.1.89/app";
-		System.out.println("Enregistrement de l'objet avec l'url : " + url);
+		// Crée 2 arraylist , une pour les zones d'échange instanciés et une
+		// autre pour les acteurs instanciés
 
-		CoeurBase app = new CoeurBase("silvemax", "12345");
-		try {
-			System.out.println("Lancement serveur");
-			Naming.rebind("app", app);
-			System.out.println("serveur lancé");
-		} catch (RemoteException | MalformedURLException e) {
-			e.printStackTrace();
-		}
+		System.setProperty("java.rmi.server.hostname", hostname); 
+		System.setProperty("java.rmi.server.codebase", "file:/" + cwd + "/bin");
+		System.out.println("codebase = " + System.getProperty("java.rmi.server.codebase"));
+
+		System.setProperty("java.security.policy", "file:/" + cwd + "/bin/policy_file");
+		System.out.println("policy = " + System.getProperty("java.security.policy"));
+
+		_ServicesCoeur skeleton = (_ServicesCoeur) new CoeurBase("admin","admin");
+		LocateRegistry.createRegistry(1099);
+		Naming.rebind("Together", skeleton); // publie notre instance
+
+
+		/*
+		 * String cwd = System.getProperty("user.dir");
+		 * 
+		 * System.setProperty("java.rmi.server.hostname", "193.48.34.19");
+		 * System.setProperty("java.rmi.server.codebase", "file://" + cwd +
+		 * "/"); System.setProperty("java.security.policy", "file://" + cwd +
+		 * "/bin/policy");
+		 * 
+		 * CoeurBase app = new CoeurBase("silvemax", "12345");
+		 * LocateRegistry.createRegistry(1099); try { System.out.println(
+		 * "Lancement serveur"); Naming.rebind("App", app); System.out.println(
+		 * "serveur lancé"); } catch (RemoteException | MalformedURLException e)
+		 * { e.printStackTrace(); }
+		 */
 
 	}
 }
