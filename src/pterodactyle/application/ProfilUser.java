15167f9cbf50548c112d2e6613e727880f46238c
unknown <fanny.vignal@e.ujf-grenoble.fr>
2016-04-29 11:01:24 +0200

    profil user

--- src/pterodactyle/application/ProfilUser.java
+++ src/pterodactyle/application/ProfilUser.java
@@ -6,6 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 
 public class ProfilUser extends JFrame {
 
@@ -31,12 +40,101 @@ public class ProfilUser extends JFrame {
 	 * Create the frame.
 	 */
 	public ProfilUser() {
+		setTitle("Profil ");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ProfilUser.class.getResource("/pterodactyle/application/ressourcesImages/logoSizeFunkySkeleton.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 585, 269);
 		contentPane = new JPanel();
+		contentPane.setForeground(new Color(11,29,62));
+		contentPane.setBackground(new Color(244,244,243));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblLogin = new JLabel("Login de l'utilisateur");
+		lblLogin.setForeground(new Color(11,29,62));
+		lblLogin.setFont(new Font("Book Antiqua", Font.BOLD, 20));
+		
+		JLabel lblNomUser = new JLabel("Nom de l'utilisateur");
+		lblNomUser.setForeground(new Color(11, 29, 62));
+		lblNomUser.setFont(new Font("Book Antiqua", Font.BOLD, 13));
+		
+		JLabel lblPrenomUser = new JLabel("Prénom de l'utilisateur");
+		lblPrenomUser.setForeground(new Color(11, 29, 62));
+		lblPrenomUser.setFont(new Font("Book Antiqua", Font.BOLD, 13));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(244,244,243));
+		
+		JButton btnNewButton = new JButton("Retour");
+		btnNewButton.setBackground(new Color(11,29,62));
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setFont(new Font("Book Antiqua", Font.BOLD, 13));
+		
+		JButton btnSuppUserAdmin = new JButton("Supprimer");
+		btnSuppUserAdmin.setForeground(Color.WHITE);
+		btnSuppUserAdmin.setFont(new Font("Book Antiqua", Font.BOLD, 13));
+		btnSuppUserAdmin.setBackground(new Color(11, 29, 62));
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(173)
+					.addComponent(lblLogin, GroupLayout.PREFERRED_SIZE, 239, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(147, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(21)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNomUser, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblPrenomUser, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED, 140, Short.MAX_VALUE)
+							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 215, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(btnNewButton)
+							.addPreferredGap(ComponentPlacement.RELATED, 306, Short.MAX_VALUE)
+							.addComponent(btnSuppUserAdmin, GroupLayout.PREFERRED_SIZE, 139, GroupLayout.PREFERRED_SIZE)
+							.addGap(20))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(9)
+					.addComponent(lblLogin)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblNomUser, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
+							.addGap(44)
+							.addComponent(lblPrenomUser, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
+							.addGap(32))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(panel, 0, 0, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnNewButton)
+						.addComponent(btnSuppUserAdmin, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(ProfilUser.class.getResource("/pterodactyle/application/ressourcesImages/SizedFunkySkeletoAppn.png")));
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
+					.addGap(27)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 95, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		panel.setLayout(gl_panel);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
